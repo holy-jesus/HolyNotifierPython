3e747ca1a59f2ceb2ae4d6e71c4161526ff57d0b
holy-jesus <hj@holy-coder.ru>
2023-08-12 15:31:29 +0500

    Перенёс все файлы в HolyNotifier

---
+++ HolyNotifier/detabase.py
@@ -0,0 +1,88 @@
+from os import getenv
+from urllib.parse import quote
+
+from utils import get_session
+
+
+class Base:
+    def __init__(self, base_name: str) -> None:
+        # I know that https://github.com/jnsougata/deta exists, but i like to do all by myself :)  
+        self.session = None
+        self.project_key = getenv("DETA_PROJECT_KEY", "")
+        self.project_id = self.project_key.split("_")[0]
+        self.base_name = base_name
+
+    async def put(self, items: list[dict]):
+        # https://deta.space/docs/en/build/reference/http-api/base#put-items
+        if isinstance(items, dict):
+            items = [items]
+        return await self.make_api_request("PUT", "items", json={"items": items})
+
+    async def get(self, key: str) -> dict:
+        # https://deta.space/docs/en/build/reference/http-api/base#get-item
+        response = await self.make_api_request("GET", f"items/{quote(key)}")
+        return response if len(response) > 1 else None
+
+    async def delete(self, key: str) -> None:
+        # https://deta.space/docs/en/build/reference/http-api/base#delete-item
+        await self.make_api_request("DELETE", f"items/{quote(key)}")
+
+    async def update(
+        self,
+        key: str,
+        *,
+        set: dict = None,
+        increment: dict = None,
+        append: dict = None,
+        prepend: dict = None,
+        delete: list[str] = None,
+    ):
+        # https://deta.space/docs/en/build/reference/http-api/base#update-item
+        payload = {}
+        if set:
+            payload["set"] = set
+        if increment:
+            payload["increment"] = increment
+        if append:
+            payload["append"] = append
+        if prepend:
+            payload["prepend"] = prepend
+        if delete:
+            payload["delete"] = delete
+        return await self.make_api_request(
+            "PATCH", f"items/{quote(key)}", json=payload or None
+        )
+
+    async def query(self, query: list = None, limit: int = None, last: str = None):
+        # https://deta.space/docs/en/build/reference/http-api/base#query-items
+        # https://deta.space/docs/en/build/reference/deta-base/queries
+        payload = {}
+        if query:
+            payload["query"] = query
+        if limit:
+            payload["limit"] = limit
+        if last:
+            payload["last"] = last
+        return await self.make_api_request("POST", "query", json=payload or None)
+
+    async def make_api_request(
+        self, method: str, endpoint: str, json: dict = None
+    ) -> dict:
+        if self.session is None:
+            self.session = await get_session()
+        response = await self.session.request(
+            method,
+            f"https://database.deta.sh/v1/{self.project_id}/{self.base_name}/{endpoint}",
+            headers={"X-API-Key": self.project_key, "Content-Type": "application/json"},
+            json=json,
+        )
+        return await response.json()
+
+
+if __name__ == "__main__":
+    import asyncio
+
+    async def main():
+        base = Base("dev_config")
+
+    asyncio.run(main())
---
+++ HolyNotifier/main.py
@@ -0,0 +1,96 @@
+import asyncio
+import secrets
+import string
+import traceback
+from os import getenv, environ
+
+import aiofiles
+from deta import Base
+from fastapi import FastAPI, Request, Response
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import HTMLResponse
+
+from utils import get, escape_symbols
+
+from twitch import Twitch
+twitch = Twitch(get("Client_Id"), get("Client_Secret"))
+
+from telegram import Telegram
+telegram = Telegram(get("Telegram_Token"))
+
+
+app = FastAPI()
+app.mount("/static", StaticFiles(directory="./static"), name="static")
+
+config = Base("dev_config" if "ngrok" in getenv("DETA_SPACE_APP_HOSTNAME") else "config")
+if not getenv("secret", None):
+    secret = config.get("secret")
+    if not secret:
+        alphabet = string.ascii_letters + string.digits
+        secret = "".join(secrets.choice(alphabet) for i in range(99))
+        config.put(secret, "secret")
+    else:
+        secret = secret["value"]
+    environ["secret"] = secret
+
+
+@app.get("/")
+async def index():
+    if not telegram.token and telegram.get_telegram_token():
+        await telegram.subscribe()
+    if not twitch.client_id or not twitch.client_secret and twitch.get_client_id_and_client_secret():
+        await twitch.subscribe()
+    async with aiofiles.open("index.html", "r") as f:
+        return HTMLResponse(await f.read())
+
+
+@app.post("/twitchwebhook")
+async def twitchwebhook(request: Request, response: Response):
+    try:
+        return await twitch.process_event(request, response)
+    except Exception as e:
+        name = str(e)
+        text = "".join(traceback.format_tb(e.__traceback__))
+        print(name, "\n", text)
+        chat_id = get("Telegram_Id")
+        if chat_id:
+            await telegram.send_message(
+                chat_id,
+                f"Exception occurred: {escape_symbols(name)}\\.\n```{escape_symbols(text)}```",
+                parse_mode="MarkdownV2",
+            )
+    finally:
+        response.status_code = 200
+        response.init_headers()
+        return response
+
+@app.post("/telegramwebhook")
+async def telegramwebhook(request: Request, response: Response):
+    try:
+        await telegram.process_event(request)
+    except Exception as e:
+        name = str(e)
+        text = "".join(traceback.format_tb(e.__traceback__))
+        print(name, "\n", text)
+        chat_id = get("Telegram_Id")
+        if chat_id:
+            await telegram.send_message(
+                chat_id,
+                f"Exception occurred: {escape_symbols(name)}\\.\n```{escape_symbols(text)}```",
+                parse_mode="MarkdownV2",
+            )
+    finally:
+        response.status_code = 200
+        response.init_headers()
+        return response
+
+async def space_actions():
+    if twitch.client_id and twitch.client_secret:
+        await twitch.subscribe()
+    if telegram.token:
+        await telegram.subscribe()
+
+if "ngrok" in getenv("DETA_SPACE_APP_HOSTNAME"):
+    loop = asyncio.get_event_loop()
+    asyncio.set_event_loop(loop)
+    loop.create_task(space_actions())
